a6766c63e92e208a6715c55132da9272f8113c21
Luis E. González <luisegr149@gmail.com>
2016-08-26 06:42:28 -0500

    Nada por agregar

---
+++ test/controller.js
@@ -0,0 +1,11 @@
+myView = angular.module("test", ['angular-magic-table']);
+myView.controller('test-controller', ['$scope','$http', function($scope,$http){
+  $scope.test = "Hola!";
+  // $http.get('https://www.mexkey.net/api/v1/showTickets.php').then(function(response){
+  $http.get('https://www.mexkey.net/api/v1/showTransactions.php').then(function(response){
+    $scope.datos = response.data;
+  });
+  $scope.magicConfig = {
+    show: 10
+  }
+}]);
