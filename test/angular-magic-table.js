a1e19a9896950e280f25ae433ed4d4bf25bf8206
Luis E. González <luisegr149@gmail.com>
2016-08-26 06:47:18 -0500

    Fixed Merge

--- test/angular-magic-table.js
+++ test/angular-magic-table.js
@@ -1,8 +1,4 @@
 magicTable = angular.module('angular-magic-table',[]);
-<<<<<<< HEAD
-=======
-
->>>>>>> fdb519573dd1ecb24760420aa26541e2eb4a50e8
 magicTable.directive('magicGrid', function() {
   return {
     restrict: 'E',
@@ -11,14 +7,10 @@ magicTable.directive('magicGrid', function() {
       config: "="
     },
     transclude: true,
-<<<<<<< HEAD
-    templateUrl: 'templates/mg-table.html'
-=======
     templateUrl: 'templates/mg-table.html',
     controller: ['$scope', function magicController($scope){
       $scope.search_text = "";
       console.log($scope.config);
     }]
->>>>>>> fdb519573dd1ecb24760420aa26541e2eb4a50e8
   };
 });
--- test/controller.js
+++ test/controller.js
@@ -6,16 +6,12 @@ myView.controller('test-controller', ['$scope','$http', function($scope,$http){
     $scope.datos = response.data;
   });
   $scope.magicConfig = {
-<<<<<<< HEAD
-    show: 10
-=======
     show: 10,
     columsFormat: [
       {1: 'text'},
       {2: 'text'},
       {3: 'text'},
-      {4: 'number'}      
+      {4: 'number'}
     ]
->>>>>>> fdb519573dd1ecb24760420aa26541e2eb4a50e8
   }
 }]);
